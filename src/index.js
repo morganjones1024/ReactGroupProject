d98e98b8acd5721bfe553a96d48c426f77177cd7
rachanne22 <rachel.sickle@gmail.com>
2022-05-04 11:27:31 -0400

    starting point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Navbar from "./components/Navbar"
+import ProductList from "./components/ProductList"
+import {BrowserRouter as Router,Link, Routes, Route} from "react-router-dom"
+
+function App(){
+  return(
+    <div>
+    <Navbar></Navbar>
+    <div>
+      <ProductList> </ProductList>
+
+    </div>
+
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(<React.StrictMode>
+  
+
+
+
+</React.StrictMode>);
