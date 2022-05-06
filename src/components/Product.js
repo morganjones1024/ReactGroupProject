7fecea17fee5f7eacf11b7f30415790b3e17f9f8
rachanne22 <rachel.sickle@gmail.com>
2022-05-06 09:23:28 -0400

    added props

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,15 +4,10 @@ function Product(props) {
   return (
     <div>
       <h2> {props.name}</h2>
-      <p>"Handheld Nintendo Switch gaming at a great price"</p>
-      <h2>Price: 40 </h2>
+      <p>{props.description}</p>
+      <h2>Price: {props.price} </h2>
       <a href="https://www.qries.com/">
-        <img
-          alt="Qries"
-          src="https://www.qries.com/images/banner_logo.png"
-          width="150"
-          height="70"
-        />
+        <img alt={props.name} src={props.image} width="150" height="70" />
       </a>
     </div>
   );
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,3 +1,5 @@
+import Product from "./Product";
+
 function ProductList() {
   let productArray = [
     {
@@ -9,7 +11,7 @@ function ProductList() {
     },
     {
       name: "Biscotti Cookies",
-      price: 40,
+      price: 50,
       description:
         "Biscotti actually means “twice baked” and the cookie is indeed baked twice, resulting in its hard and crunchy texture.",
       image: "https://cdn.buttercms.com/UqbFGAJLTGqtYqF2UajV",
@@ -22,26 +24,34 @@ function ProductList() {
       image: "https://cdn.buttercms.com/UqbFGAJLTGqtYqF2UajV",
     },
     {
-      name: "Biscotti Cookies",
-      price: 40,
+      name: "Game",
+      price: 100,
       description:
         "Biscotti actually means “twice baked” and the cookie is indeed baked twice, resulting in its hard and crunchy texture.",
       image: "https://cdn.buttercms.com/UqbFGAJLTGqtYqF2UajV",
     },
     {
-      name: "Biscotti Cookies",
-      price: 40,
+      name: "Nintendo Switch Lite - Coral",
+      price: "$28.60",
       description:
-        "Biscotti actually means “twice baked” and the cookie is indeed baked twice, resulting in its hard and crunchy texture.",
-      image: "https://cdn.buttercms.com/UqbFGAJLTGqtYqF2UajV",
+        "	Handheld Nintendo Switch gaming at a great price\n For every member of your family, there’s a member of ours\nOptimized for personal, handheld play, Nintendo Switch Lite is a small and light Nintendo Switch system\nFeatures a sleek, unibody design with fully integrated controls and a built-in plus Control Pad\nCompatible with all physical and digital Nintendo Switch games that support Handheld mode",
+
+      image: "https://m.media-amazon.com/images/I/41zSn74fPuL._AC_.jpg",
     },
   ];
   let productElements = productArray.map((product) => (
     <div>
-      <img src={product.image} alt={product.name} />
-      {product.name}
+      {/* <img src={product.image} alt={product.name} />
+      name={product.name}
       {product.price}
-      {product.description}
+      {product.description} */}
+      <Product
+        name={product.name}
+        price={product.price}
+        image={product.image}
+        alt={product.name}
+        description={product.description}
+      />
     </div>
   ));
   return <>{productElements}</>;
